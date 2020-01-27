04e35217263c0698122304831b827c4eef594cb4
Guadarrama <albertc.barron@gmail.com>
2020-01-27 13:23:18 -0500

    initial complete

---
+++ client/build/precache-manifest.eb5e5b5e533d890c2f63c1ad12e871af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907442b0e88a981760b6c9c502bc815e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0ac42c345791df77f69",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ffe77e522e8e25409df4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c0ac42c345791df77f69",
+    "url": "/static/js/2.fad30a70.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fad30a70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffe77e522e8e25409df4",
+    "url": "/static/js/main.aaa5604d.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
